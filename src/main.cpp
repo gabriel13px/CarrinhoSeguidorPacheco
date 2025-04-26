085e1e3990d9763a04150d4c0b4c7d3e63089e4e
gabriel13px <gabrielga0304@gmail.com>
2025-04-26 12:11:33 -0300

    inversão de motores  inicial

---
+++ src/main.cpp
@@ -0,0 +1,281 @@
+#include <Arduino.h>
+#include <QTRSensors.h>
+#include "BluetoothSerial.h"
+QTRSensors qtr;
+const uint8_t QuantSensores = 8;
+uint16_t SensorValores[QuantSensores];
+float Kp = .2;
+float Ki = 0;
+float Kd = .1;
+int P;
+int I;
+int D;
+int UltimoErro = 0;
+boolean OnOff = false;
+BluetoothSerial SerialBT;
+int ManualPid = 0;// 0 = PID, 1 = Manual
+
+ uint8_t VelocidadeMaximaA = 255;
+ uint8_t VelocidadeMaximaB = 255;
+ uint8_t VelocidadeBaseA = 170;
+ uint8_t VelocidadeBaseB = 170
+;
+
+// os sensores devem ser colocados na ordem
+//17,18,13,14,27,26,25,33 gpios usados sensores  -entrada digital
+//32 para buzzer -saida pwm
+//5 para botao - input pullup
+//2 para led - saida digital
+//19,21,22,23 para ponte H - saidas pwm
+//16 ir led
+
+int AHorario =19;// esquerda horario
+int AAntiHora =21;//esquerda anti horario
+int BHorario =22;//direita horario 
+int BAntiHora  =23;//direita anti horario
+int LedIndicador = 2;
+
+int botao = 5; 
+
+void calibracao();
+void frente_freio(int motorA, int motorB);
+void Controle_PID();
+void PiscaPisca(int tempo, int vezes);
+
+void setup(){
+  Serial.begin(115200);
+  SerialBT.begin("Pacheco"); 
+  ledcSetup(0, 5000, 8);
+  ledcSetup(1, 5000, 8);
+  ledcSetup(2, 5000, 8);
+  ledcSetup(3, 5000, 8);
+
+  qtr.setTypeRC();
+  qtr.setSensorPins((const uint8_t[]){17,18,13,14,27,26,25,33}, QuantSensores);
+  qtr.setEmitterPin(16); // IR Pin
+
+
+  ledcAttachPin(AHorario, 0);
+  ledcAttachPin(BAntiHora, 1);
+  ledcAttachPin(AAntiHora, 2);
+  ledcAttachPin(BHorario, 3);
+
+ 
+  pinMode(botao, INPUT_PULLUP);
+  pinMode(LedIndicador, OUTPUT);
+  delay(500);
+  
+  boolean Espera = false;
+  while (Espera == false) {
+    if(digitalRead(botao) == LOW) {
+      calibracao();
+      Espera = true;
+    }
+  }
+  frente_freio(0,0);
+}
+
+void loop(){
+  //verifica se o bluetooth está conectado e faz a troca das constantes outroca para o modo manual
+  if(SerialBT.available()){
+    String EntradaSerial = SerialBT.readStringUntil(' '); // Lê até Enter
+    Serial.println("Recebido: " + EntradaSerial);
+    EntradaSerial.trim();
+
+     if (EntradaSerial.length() >= 2) {
+      String modo = EntradaSerial.substring(0, 2);
+
+      if (modo == "MA") {
+        ManualPid = 1;
+        Serial.println("Modo Manual ativado");
+      } else if (modo == "PI") {
+        ManualPid = 0;
+        Serial.println("Modo PID ativado");
+      }
+    }
+    
+    //leitura e atualização de constantes
+    int KpIndex = EntradaSerial.indexOf("Kp=");
+    int KiIndex = EntradaSerial.indexOf("Ki=");
+    int KdIndex = EntradaSerial.indexOf("Kd=");
+    int VMaxIndex = EntradaSerial.indexOf("Vmax=");
+    int VMinIndex = EntradaSerial.indexOf("Vmin=");
+    String VMaxStr = EntradaSerial.substring(VMaxIndex + 5, VMinIndex);
+    String VMinStr = EntradaSerial.substring(VMinIndex + 5);
+    VelocidadeMaximaA = (uint8_t)VMaxStr.toInt();
+    VelocidadeMaximaB = (uint8_t)VMaxStr.toInt();
+    VelocidadeBaseA = (uint8_t)VMinStr.toInt();
+    VelocidadeBaseB = (uint8_t)VMinStr.toInt();
+
+    if (KpIndex >= 0 && KiIndex > KpIndex && KdIndex > KiIndex && ManualPid==0) {
+      String kpStr = EntradaSerial.substring(KpIndex+3, KiIndex - 1);
+      String kiStr = EntradaSerial.substring(KiIndex + 3, KdIndex - 1);
+      String kdStr = EntradaSerial.substring(KdIndex + 3, VMaxIndex);
+      
+      Kp = kpStr.toFloat();
+      Ki = kiStr.toFloat();
+      Kd = kdStr.toFloat();
+
+      Serial.printf("Constantes atualizadas:\nKp = %.2f\nKi = %.2f\nKd = %.2f\n", Kp, Ki, Kd);
+    } 
+    // leitura e modo manual
+    if (ManualPid == 1 && EntradaSerial.length() >= 3) {
+      char comando = EntradaSerial.charAt(2);
+      int DelIndex = EntradaSerial.indexOf("Del=");
+      String DelStr = EntradaSerial.substring(DelIndex + 4,VMaxIndex);
+      int Del = DelStr.toInt();
+    
+      switch (comando) {
+        case 'F': // Frente
+          frente_freio(VelocidadeMaximaA, VelocidadeMaximaB);
+          Serial.println("Frente");
+          if (Del > 0) {
+            delay(Del);
+            frente_freio(0, 0);
+          }
+          break;
+    
+        case 'T': // Trás
+          frente_freio(-VelocidadeMaximaA, -VelocidadeMaximaB);
+          Serial.println("Tras");
+          if (Del > 0) {
+            delay(Del);
+            frente_freio(0, 0);
+          }
+          
+          break;
+    
+        case 'L': // Esquerda
+          frente_freio(VelocidadeMaximaA, -VelocidadeMaximaB);
+          Serial.println("Esquerda");
+          if (Del > 0) {
+            delay(Del);
+            frente_freio(0, 0);
+          }
+          break;
+    
+        case 'R': // Direita
+          frente_freio(-VelocidadeMaximaA, VelocidadeMaximaB);
+          Serial.println("Direita");
+          if (Del > 0) {
+            delay(Del);
+            frente_freio(0, 0);
+          }
+          break;
+    
+        default: 
+          frente_freio(0, 0);
+          Serial.println("Parado");
+          break;
+      }
+    }
+
+  }
+
+  //botão liga e desliga do carro
+  if(digitalRead(botao)==LOW && ManualPid == 0){
+    OnOff = !OnOff;
+    if(OnOff == true) {
+      //tempo até o carro ligar
+      PiscaPisca(400, 4);
+      Serial.println("corrida iniciada");
+    }
+    else {
+      
+      //tempo até o carro desligar
+      PiscaPisca(200, 2);
+      Serial.println("corrida Acabou");
+    }
+  }
+
+  //controle liga e desliga do carro
+  if (OnOff == true&& ManualPid == 0){
+    Controle_PID();
+  }else{
+    if (ManualPid == 0) {
+      frente_freio(0, 0);
+    }
+  }
+}
+
+//controle dos motores
+void frente_freio(int motorA, int motorB){
+  if(motorA > 0){
+    ledcWrite(0, motorA);
+    ledcWrite(2, 0);
+  }else if(motorA < 0){
+    ledcWrite(0, 0);
+    ledcWrite(2, -motorA);
+  }else{
+    ledcWrite(0, 0);
+    ledcWrite(2, 0);
+  }
+  if(motorB > 0){
+    ledcWrite(1, motorB);
+    ledcWrite(3, 0);
+  }else if(motorA < 0){
+    ledcWrite(1, 0);
+    ledcWrite(3, -motorB);
+  }else{
+    ledcWrite(1, 0);
+    ledcWrite(3, 0);
+  }
+}
+
+
+
+//controle do PID
+void Controle_PID(){
+uint16_t posicao = qtr.readLineBlack(SensorValores);
+int erro = 3500 - posicao;
+//Serial.println(posicao);
+P = erro;
+I = I + erro;
+D = erro - UltimoErro;
+UltimoErro = erro;
+
+//vai ser necessario alterar a logica dos motores
+int VelocidadeMotor = P*Kp + I*Ki + D*Kd;
+int VelocidadeA = VelocidadeBaseA - VelocidadeMotor;
+int VelocidadeB = VelocidadeBaseB + VelocidadeMotor;
+
+
+if (VelocidadeA > VelocidadeMaximaA) {
+  VelocidadeA = VelocidadeMaximaA;
+}
+if (VelocidadeB > VelocidadeMaximaB) {
+  VelocidadeB = VelocidadeMaximaB;
+}
+if (VelocidadeA < -VelocidadeMaximaA) {
+  VelocidadeA = -VelocidadeMaximaA;
+}
+if (VelocidadeB < -VelocidadeMaximaB) {
+  VelocidadeB = -VelocidadeMaximaB;
+}
+frente_freio(VelocidadeA, VelocidadeB);
+Serial.println(VelocidadeA);
+Serial.println(VelocidadeB);
+Serial.println(posicao);
+}
+
+
+void calibracao() {
+  Serial.println("Calibrando...");
+  digitalWrite(LedIndicador, HIGH);
+  for (uint16_t i = 0; i < 400; i++) {
+    qtr.calibrate();
+  }
+  Serial.println("Calibracao concluida");
+  delay(1000);
+  digitalWrite(LedIndicador, LOW);
+}
+
+// pisca o led indicador
+void PiscaPisca(int tempo, int vezes) {
+  for (int i = 0; i < vezes; i++) {
+    digitalWrite(LedIndicador, HIGH);
+    delay(tempo);
+    digitalWrite(LedIndicador, LOW);
+    delay(tempo);
+  }
+}
